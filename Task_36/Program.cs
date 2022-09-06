333d1fdb9af26d9e089dd6cf4a6b5a9f83db79f0
Nikita Lunev <lunevbros@yandex.ru>
2022-09-07 02:21:56 +0300

    complete homework 5

---
+++ Task_36/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+void FillArray(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+}
+
+int[] arr = new int[6];
+FillArray(arr);
+Console.WriteLine("Программа создала массив: " + "[{0}]", String.Join(", ", arr));
+
+int length = arr.Length;
+int result = 0;
+
+for (int i = 1; i < length; i = i + 2)
+{
+    result = result + arr[i];
+}
+
+Console.WriteLine("Сумма нечетных чисел созданного массива равна: " + result);
---
+++ Task_38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+void FillArray(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+}
+
+int[] arr = new int[6];
+FillArray(arr);
+Console.WriteLine("Программа создала массив: " + "[{0}]", String.Join(", ", arr));
+
+
+int size = arr.Length;
+int max = 0;
+int min = arr[0];
+for (int i = 0; i < size; i++)
+{
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+}
+
+Console.WriteLine("Максимальный элемент массива: " + max);
+Console.WriteLine("Минимальный элемент массива: " + min);
+Console.WriteLine("Разница между максимальным и минимальным элементом массива равна: " + (max - min));
